f965a73a2fbb1b6fc152309f4ae1a7a9036ee9a9
Han <hieuhantr@gmail.com>
2023-04-26 15:01:44 -0700

    Finish Project

--- script.js
+++ script.js
@@ -1,23 +1,43 @@
+//query container & slider
+const slider = document.querySelector('#slider')
+const slidercontainer = document.querySelector('#slidercontainer')
+console.log(slider)
+//create output
+const output = document.createElement('output')
+output.innerHTML = slider.value + ' x ' + slider.value
+slidercontainer.insertBefore(output, slider)
+
+//query grid container
 const container = document.querySelector("#container");
+//style and create grid
 container.style.display = 'grid';
-container.style['grid-template-columns'] = 'repeat(16, 1fr)';
-container.style['grid-template-rows'] = 'repeat(16, 1fr)';
-console.log(container)
 
-for (var row = 1; row <= 16; row++) {
-    for (var column = 1; column <= 16; column++) {
-        let square = document.createElement('div')
-        square.classList.add('square')
-        square.style.backgroundColor = 'lightgreen'
-        container.appendChild(square)
+//make grid & hover event
+const makeGrid = () => {
+    container.style['grid-template-columns'] = `repeat(${slider.value}, 1fr)`
+    container.style['grid-template-rows'] = `repeat(${slider.value}, 1fr)`
+    for (var row = 1; row <= slider.value; row++) {
+        for (var column = 1; column <= slider.value; column++) {
+            let square = document.createElement('div')
+            square.classList.add('square')
+            square.style.backgroundColor = 'lightblue'
+            container.appendChild(square)
+        }
     }
-}
-
-const allSquares = document.querySelectorAll('.square')
-allSquares.forEach( square => {
-    square.addEventListener("mouseover", () => {
-        square.style.backgroundColor = 'black'
+    const allSquares = document.querySelectorAll('.square')
+    allSquares.forEach(square => {
+        square.addEventListener("mouseover", () => {
+            square.style.backgroundColor = 'blue'
+        })
     })
-})
+}
+makeGrid();
 
+//make output dynamic
+const handleChange = () => {
+    output.innerHTML = slider.value + ' x ' + slider.value;
+    container.innerHTML = '';
+    makeGrid();
+}
+slider.addEventListener('input', handleChange)
 
